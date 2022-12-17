1a5e05b50f6122f3df9773bc17af065e9d1a241a
YaroslavKustol <kyst-007@mail.ru>
2022-12-17 16:26:38 +0800

    Added 1,2,3,4

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//============================================================================================================
+// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+//============================================================================================================
+
+string? inputLineA = Console.ReadLine(); //Читаем данные пользователя
+string? inputLineB = Console.ReadLine();
+if (inputLineA != null && inputLineB != null) //Проверка на отсутсвие значения. Предотвращение ошибки
+{
+    int inputNumberA = int.Parse(inputLineA); //Перевод строки в вещественное число
+    int inputNumberB = int.Parse(inputLineB);
+
+    if (inputNumberA > inputNumberB)
+    {
+        Console.WriteLine("Число A > B");
+    }
+    else
+    {
+        Console.WriteLine("Число B > A");
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿// ===================================================================
+// Задача 2: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// ===================================================================
+Console.WriteLine("Введите число А: ");
+string inputLineA = Console.ReadLine() ?? ""; //Читаем данные пользователя
+Console.WriteLine(string.Empty);
+Console.WriteLine("Введите число B: ");
+string inputLineB = Console.ReadLine() ?? "";//Проверка на отсутсвие значения. Предотвращение ошибки знаки:(??"")
+Console.WriteLine(string.Empty);
+Console.WriteLine("Введите число C: ");
+string inputLineC = Console.ReadLine() ?? "";
+Console.WriteLine(string.Empty);
+
+int A = int.Parse(inputLineA); //Перевод строки в вещественное число
+int B = int.Parse(inputLineB);
+int C = int.Parse(inputLineC);
+
+
+if (A > B) //(inputNumberA > inputNumberB && inputNumberA > inputNumberC ) ?
+{
+    if (A > C)
+
+        Console.WriteLine("Max A");
+    else
+        Console.WriteLine("Max C");
+}
+
+else
+{
+    if (B > C)
+
+        Console.WriteLine("Max B");
+    else
+        Console.WriteLine("Max C");
+}
+
+int max = 0;
+
+int D = Math.Max(A, B);
+int finallymax = Math.Max(max, C);
+Console.WriteLine($"Максимальное число {finallymax}");
