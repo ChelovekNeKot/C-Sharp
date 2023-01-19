a9de8dd683ba09f24fae745fb32d017aa86698c5
YaroslavKustol <kyst-007@mail.ru>
2023-01-19 21:24:35 +0800

    Added HV_5

---
+++ Seminar_5/Task_1/Program.cs
@@ -0,0 +1,81 @@
+﻿// ========================================================================================
+//  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+// ========================================================================================
+
+// Универсальный метод генерации и заполнение массива
+int[] FillArray(int length,int arrMin, int arrMax)
+{
+    Random rand = new Random();   //Генератор случайных чисел
+    int[] array = new int[length]; //Создаем массив
+    for (int i = 0; i < array.Length; i++) //Заполняем массив
+    {
+        array[i] = rand.Next(arrMin, arrMax + 1);
+    }
+    return BubbbleSort(array);
+}
+
+// печатаем массив
+void Print1DArray(int[] arr)
+{
+    Console.Write("[" + arr[0] + ", ");
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+int EvenNumberers(int[] arr)
+{
+    int count = 0;//переменная накопления
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (EvenTest(arr[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+bool EvenTest(int count)
+{
+    return (count % 2 == 0);
+
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int[] BubbbleSort(int[] array) // сортировка пузырьком, чтобы было "красиво"
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[i] > array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[] array = FillArray(10, 100, 999); //заполняем массив  = вызов метод массива(передаем параметры)
+Print1DArray(array); // печать массива, чтобы увидеть, что всё правильно
+
+int countEven = EvenNumberers(array);
+
+PrintResult($"Количество чётных чисел в массиве: {countEven}");
+
---
+++ Seminar_5/Task_2/Program.cs
@@ -0,0 +1,77 @@
+﻿//====================================================================== 
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+// =====================================================================
+
+// //Универсальный метод генерации и заполнения массива
+int[] FillArray(int num, int arrMin, int arrMax)
+{
+    //Генератор случайных чисел
+    Random numSintezator = new Random();
+    //Создаем массив
+    int[] arr = new int[num];
+    //Тест границ
+    if (arrMin < arrMax)
+    {
+        //Заполняем массив
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = numSintezator.Next(arrMin, arrMax + 1);
+        }
+    }
+    //Возвращаем результат
+    return arr;
+}
+
+// печатаем массив
+void Print1DArray(int[] arr)
+{
+    Console.Write("[" + arr[0] + ", ");
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+int SumNeChetEl(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i = i + 2)
+    {
+        count += arr[i];    
+    }
+    return count;
+}
+
+int SumNeChetIndex(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i = i + 1)
+    {
+        count += arr[i];    
+    }
+    return count;
+}
+
+int[] array = FillArray(10, -10, 10); //заполняем массив  = вызов метод массива(передаем параметры)
+Print1DArray(array); // печать массива, чтобы увидеть, что всё правильно
+Console.WriteLine();
+int summa = SumNeChetEl(array);
+int summa1 = SumNeChetIndex(array);
+
+PrintResult($"Сумма элементов на нечётных позициях: {summa}"); // Выводим сумму элементов из нечетных позиций
+Console.WriteLine();
+PrintResult($"Сумма элементов на нечётных index: {summa1}"); // Выводим сумму элементов из нечетных index
+
